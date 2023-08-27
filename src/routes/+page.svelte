2ba4715dd4f77000635284d0046e25e62ded5178
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2023-08-27 14:59:57 -0700

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    
+</script>
+    
+<img class="bkg" src="/bkg.png" alt="">
+    
+<style>
+    .bkg {
+        width: 100%;
+    }
+</style>
