87f22cb09720612d1d58a916b7e14935009e446e
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2023-09-04 22:03:26 -0700

    hehe

---
+++ src/lib/bouncy.svelte
@@ -0,0 +1,145 @@
+<script lang="ts">
+    import { create } from "canvas-confetti";
+    let canvas: HTMLCanvasElement;
+    let canvas2: HTMLCanvasElement;
+    let myConfetti: any;
+    let myConfetti2: any;
+    setTimeout(() => {
+        myConfetti = create(canvas, {
+            resize: true,
+            useWorker: true,
+        });
+        myConfetti2 = create(canvas2, {
+            resize: true,
+            useWorker: true,
+        });
+    });
+</script>
+
+<canvas bind:this={canvas2} />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+    class="bouncy"
+    on:click={() => {
+        myConfetti({
+            particleCount: 100,
+            spread: 160,
+            // any other options from the global
+            // confetti function
+        });
+        setTimeout(() => {
+            myConfetti2({
+                particleCount: 100,
+                spread: 160,
+                // any other options from the global
+                // confetti function
+            });
+        }, 1000);
+    }}
+>
+    <canvas bind:this={canvas} />
+    <img src="/stout.jpg" alt="stout" srcset="" />
+    <p>click me!</p>
+</div>
+
+<style>
+    canvas {
+        position: absolute;
+        z-index: 2000;
+
+        width: 100%;
+        height: 100%;
+    }
+    p {
+        background-color: white;
+        position: absolute;
+        top: 20px;
+    }
+    /* vanilla css dvd-style bouncing animation */
+    img {
+        width: 100%;
+        height: 100%;
+    }
+    .bouncy {
+        display: block;
+        position: absolute;
+        width: calc(90px + 10vmin);
+        height: calc(90px + 10vmin);
+        -webkit-animation: moveX 2s linear 0s infinite alternate,
+            moveY 3.4s linear 0s infinite alternate;
+        -moz-animation: moveX 2s linear 0s infinite alternate,
+            moveY 3.4s linear 0s infinite alternate;
+        -o-animation: moveX 2s linear 0s infinite alternate,
+            moveY 3.4s linear 0s infinite alternate;
+        animation: moveX 2s linear 0s infinite alternate,
+            moveY 3.4s linear 0s infinite alternate;
+    }
+
+    @-webkit-keyframes moveX {
+        from {
+            left: 0;
+        }
+        to {
+            left: calc(100vw - (90px + 10vmin));
+        }
+    }
+    @-moz-keyframes moveX {
+        from {
+            left: 0;
+        }
+        to {
+            left: calc(100vw - (90px + 10vmin));
+        }
+    }
+    @-o-keyframes moveX {
+        from {
+            left: 0;
+        }
+        to {
+            left: calc(100vw - (90px + 10vmin));
+        }
+    }
+    @keyframes moveX {
+        from {
+            left: 0;
+        }
+        to {
+            left: calc(100vw - (90px + 10vmin));
+        }
+    }
+
+    @-webkit-keyframes moveY {
+        from {
+            top: 0;
+        }
+        to {
+            top: calc(100vh - (120px + 10vmin));
+        }
+    }
+    @-moz-keyframes moveY {
+        from {
+            top: 0;
+        }
+        to {
+            top: calc(100vh - (120px + 10vmin));
+        }
+    }
+    @-o-keyframes moveY {
+        from {
+            top: 0;
+        }
+        to {
+            top: calc(100vh - (120px + 10vmin));
+        }
+    }
+    @keyframes moveY {
+        from {
+            top: 0;
+        }
+        to {
+            top: calc(100vh - (120px + 10vmin));
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,50 @@
 <script lang="ts">
-    
+    import Bouncy from "./../lib/bouncy.svelte";
 </script>
-    
-<img class="bkg" src="/bkg.png" alt="">
-    
+
+<Bouncy />
+<img class="bkg" src="/bkg.png" alt="" />
+<!-- a fixed bottom white bar with black top outline with button to sign up -->
+<div class="bottom-bar">
+    <div class="p-2">
+        <button
+            class="btn"
+            on:click={() => {
+                window.location.href =
+                    "https://bsf8x7mgm2l.typeform.com/to/xOLsX7CJ";
+            }}>RSVP</button
+        >
+    </div>
+</div>
+
 <style>
     .bkg {
         width: 100%;
         max-width: 120vh;
+        box-shadow: inset 0 0 0 2000px rgb(0, 0, 0);
+    }
+    .btn {
+        border-radius: 0;
+        border-color: black;
+        max-height: 10vw;
+        height: 5vh;
+        width: calc(20px + 10vw);
+    }
+    .bottom-bar {
+        position: fixed;
+        padding: 20px;
+        bottom: 20px;
+        width: calc(100% - 80px);
+        left: 20px;
+        max-width: calc(120vh - 80px);
+        border-color: white;
+        display: flex;
+        justify-content: right;
+        align-items: center;
+        /* makes background b&w checkerboard */
+        background: repeating-conic-gradient(#ffffffc9 0% 25%, #233739 0% 50%)
+            50% / 20px 20px;
+
+        z-index: 5000;
     }
 </style>
