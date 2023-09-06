4a5846b2ad7c062964427c47844195f146a9653b
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2023-09-06 12:51:56 -0700

    idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,131 @@
 <script lang="ts">
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
     import Bouncy from "./../lib/bouncy.svelte";
-    let stout = true;
+    let stout = false;
+
+    // let scrollw = 0;
+    // let normalw = 0;
+
+    // const checkScrollBars = () => {
+    //     if (!browser) {
+    //         console.log("skipping");
+    //         return;
+    //     }
+    //     const b = document.body;
+
+    //     normalw = window.innerWidth;
+    //     scrollw = normalw - b.clientWidth;
+    //     console.log(scrollw);
+    // };
+    // onMount(() => {
+    //     checkScrollBars();
+    //     console.log("mounted");
+    // });
+    // if (browser) window.onresize = checkScrollBars;
 </script>
 
 {#if stout}
     <Bouncy />
 {/if}
 <img class="logo" src="/whole-logo.svg" alt="logo" srcset="" />
+<div class="nav">
+    <button
+        class="schedule-button"
+        on:click={() => {
+            alert("no schedule yet");
+        }}
+    >
+        <img src="/schedule-button.svg" class="schedule-icon" alt="" />
+    </button>
+    <button
+        class="menu-button"
+        on:click={() => {
+            stout = !stout;
+        }}
+    >
+        <img src="/menu-button.svg" class="menu-icon" alt="" />
+    </button>
+</div>
 <img class="bkg" src="/bkg.png" alt="" />
-<!-- a fixed bottom white bar with black top outline with button to sign up -->
-<div class="bottom-bar">
-    <div class="p-2">
-        <button
-            class="btn"
-            on:click={() => {
-                window.location.href =
-                    "https://bsf8x7mgm2l.typeform.com/to/xOLsX7CJ";
-            }}>RSVP</button
-        >
-    </div>
+
+<div class="body">
+    <p>
+        Welcome to HackHW's Game Jam – a 48-hour programming marathon judged by
+        a panel of alumni in tech
+    </p>
 </div>
 
 <style>
+    p {
+        font-family: "Inter-SemiBold", monospace;
+        font-size: 1.3rem;
+        color: #9a9a9a;
+        text-align: center;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .body {
+        margin-top: 30px;
+
+        height: 100vh;
+        width: 85%;
+        max-width: 80vh;
+        display: block;
+        margin: auto;
+    }
+
+    .schedule-icon {
+        height: 100%;
+    }
+    .schedule-button {
+        height: 45px;
+        background-color: transparent;
+        padding: 0px;
+
+        border: 0px solid transparent;
+
+        margin-right: 7px;
+    }
+    .menu-icon {
+        width: 100%;
+        height: 100%;
+    }
+    .menu-button {
+        width: 45px;
+        height: 45px;
+        margin-inline: 7px;
+
+        border-radius: 100%;
+        border: 0px;
+
+        background-color: transparent;
+
+        padding: 0px;
+    }
+    .nav {
+        top: 0px;
+        right: 0px;
+        height: 60px;
+        width: 100vw;
+
+        position: fixed;
+
+        display: flex;
+        align-items: center;
+        justify-content: right;
+    }
     .logo {
         left: 0px;
         top: 0px;
         position: fixed;
-        width: 20vw;
+        width: 40vw;
         max-width: 170px;
     }
     .bkg {
+        margin-top: 30px;
         width: 100%;
         box-shadow: inset 0 0 0 2000px rgb(0, 0, 0);
-    }
-    .btn {
-        border-radius: 0;
-        border-color: black;
-        max-height: 10vw;
-        height: 5vh;
-        width: calc(20px + 10vw);
-    }
-    .bottom-bar {
-        position: fixed;
-        padding: 20px;
-        bottom: 20px;
-        width: calc(100% - 80px);
-        left: 20px;
-        max-width: calc(120vh - 80px);
-        border-color: white;
-        display: flex;
-        justify-content: right;
-        align-items: center;
-        /* makes background b&w checkerboard */
-        background: repeating-conic-gradient(#ffffffc9 0% 25%, #233739 0% 50%)
-            50% / 20px 20px;
-
-        z-index: 5000;
     }
 </style>
